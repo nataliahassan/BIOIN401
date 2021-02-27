8386363534f92cd36212a0062d74e4000de5a923
tylerwatson98 <32936743+tylerwatson98@users.noreply.github.com>
2021-02-27 15:32:18 -0700

    Create Viral Recall Base Script.sh

---
+++ Viral Recall Base Script.sh
@@ -0,0 +1,4 @@
+# Simple script for running viral recall on a genome
+# Example python3 viralrecall.py -i /home/tyler/B401/Data/ncbi_dataset/new_test/GCA_002158475.1_Ostta1115_2_genomic.fna -p tests2 -t 2 -f
+
+# just replace the input path for the fna sequence and output path
